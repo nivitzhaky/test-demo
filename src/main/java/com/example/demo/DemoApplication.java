eb737efc066df856fff7a5c4d4033d58fde92db3
Niv Itzhaky <niv.itzhaky@gmail.com>
2023-08-30 17:09:17 +0300

    code on b1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	// comment added
+	// comment added - 1
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
